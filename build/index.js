db93f387ea072a5e9aa134f9f7fb1f3fab5b2b5b
niklauslee <niklaus.lee@gmail.com>
2025-04-03 13:00:28 +0900

    initial commit

---
+++ build/index.js
@@ -0,0 +1,35 @@
+import { McpServer, ResourceTemplate, } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+// Create an MCP server
+const server = new McpServer({
+    name: "frame0-mcp-server",
+    version: "1.0.0",
+});
+// Add an addition tool
+server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }],
+}));
+// Add a dynamic greeting resource
+server.resource("greeting", new ResourceTemplate("greeting://{name}", { list: undefined }), async (uri, { name }) => ({
+    contents: [
+        {
+            uri: uri.href,
+            text: `Hello, ${name}!`,
+        },
+    ],
+}));
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Weather MCP Server running on stdio");
+}
+main().catch((error) => {
+    console.error("Error starting server:", error);
+    process.exit(1);
+});
+// Start receiving messages on stdin and sending messages on stdout
+// const transport = new StdioServerTransport();
+// transport.start();
+// console.log("Listening for messages on stdin and sending messages on stdout");
+// await server.connect(transport);
